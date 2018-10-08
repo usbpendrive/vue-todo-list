d28baffa0f471972e3aff059322f9790a8fdfaaf
Fernando <ferry.fernando@gmail.com>
2018-10-08 08:18:49 +0700

    Add Bootstrap CSS Framework. Default todo listing.

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
 <template>
   <div id="app">
-    <h4>Vue ToDo List</h4>
+    <h4 class="bg-primary text-white text-center p-2">
+      {{name}}'s ToDo List
+    </h4>
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Task</div>
+        <div class="col font-weight-bold">Done</div>
+      </div>
+      <div class="row" v-for="t in tasks" v-bind:key="t.action">
+        <div class="col">{{t.action}}</div>
+        <div class="col-2">
+          <input type="checkbox" v-model="t.done" class="form-check-input" />
+          {{t.done}}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+    data() {
+      return {
+          name: "Fernando",
+          tasks: [
+              { action: "Buy Flowers", done: false },
+              { action: "Get Shoes", done: false },
+              { action: "Collect Tickets", done: true },
+              { action: "Call Joe", done: false }
+          ]
+      }
+    },
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
