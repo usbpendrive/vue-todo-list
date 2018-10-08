2d5356781a0928bde26da02531dcf54d6b042365
Fernando <ferry.fernando@gmail.com>
2018-10-08 08:27:56 +0700

    New todo input form.

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,14 @@
           <input type="checkbox" v-model="t.done" class="form-check-input" />
         </div>
       </div>
+      <div class="row py-2">
+        <div class="col">
+          <input v-model="newItemText" class="form-control" />
+        </div>
+        <div class="col-2">
+          <button class="btn btn-primary" v-on:click="addNewTodo">Add</button>
+        </div>
+      </div>
       <div class="row bg-secondary py-2 mt-2 text-white">
         <div class="col text-center">
           <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
@@ -38,12 +46,22 @@ export default {
               { action: "Collect Tickets", done: true },
               { action: "Call Joe", done: false }
           ],
-          hideCompleted: true
+          hideCompleted: true,
+          newItemText: ""
       }
     },
     computed: {
       filteredTasks() {
           return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks
+      }
+    },
+    methods: {
+      addNewTodo() {
+          this.tasks.push({
+              action: this.newItemText,
+              done: false
+          });
+          this.newItemText = "";
       }
     }
 }
